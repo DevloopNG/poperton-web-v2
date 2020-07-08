ae580327b0443250eb24bfa4ea7825ecdb20bd4c
phemieny7 <oyewo.oluwafemi@gmail.com>
2020-07-08 11:50:07 +0100

    The user dashboard
    My first commit regarding the user dashboard

---
+++ user dashboard/js/main.js
@@ -0,0 +1,51 @@
+$('.toggle').on('click', function () {
+    $('.menu').toggleClass('active');
+});
+
+
+//Sidebar
+(function ($) {
+
+    "use strict";
+
+    var fullHeight = function () {
+
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+
+    };
+    fullHeight();
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+})(jQuery);
+
+//Dark mode toggler
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
+
+//Datatables
+$(document).ready(function () {
+    $('#table_id').DataTable();
+});
+
